4c3966ae523a0b3d78df31b7bbc7ec0dcc879d27
PeterLZhou <peterbzhou@gmail.com>
2015-11-14 22:13:51 -0500

    Added geolocator to map and ready to implement multiple markers

--- static/js/map.js
+++ static/js/map.js
@@ -1,15 +1,18 @@
 function initMap() {
-  var myLatLng = //need data from shivam
+  navigator.geolocation.getCurrentPosition(function(response){
+    var myLatLng = {lat: response.coords.lattitude, lng: response.coords.longitude};
 
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 4,
     center: myLatLng
   });
 
-  var markers = [
-        ['London Eye, London', 51.503454,-0.119562],
-        ['Palace of Westminster, London', 51.499633,-0.124755]
-    ]/*array of arrays (title, lat, long) from shivam*/
+  var mymarker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+  });
+
+  var markers = [] /*nested arrays of every posting from shivam*/
 
   for (i = 0; i <= markers.length(); i++)
   var position = new google.maps.LatLng(markers[i].lat, markers[i].lon);
@@ -20,6 +23,7 @@ function initMap() {
             title: markers[i].title
   }
   );
+  })
 }
 
 // just testing something out
